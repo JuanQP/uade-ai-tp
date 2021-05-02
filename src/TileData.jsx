ead9b6fd423d014e64f05570c237f0ec860082f2
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-05-02 02:17:06 -0300

    Add files via upload

---
+++ src/TileData.jsx
@@ -0,0 +1,42 @@
+import image1 from './imagenes/DX-110_P1_350x250.png';
+import image2 from './imagenes/DX-125_P1_350x250.png';
+import image3 from './imagenes/KB-116_P1_350x250.png';
+import image4 from './imagenes/KB-118_P1_350x250.png';
+import image5  from './imagenes/SW-21-370_P1_350x250.jpg';
+import image6  from './imagenes/SW-21-375_P1_350x250.jpg';
+
+  const tileData = [
+    {
+      img: image1,
+      title: 'Mouse Genius',
+      model: 'DX-110',
+    },
+    {
+      img: image2,
+      title: 'Mouse Genius',
+      model: 'DX-125',
+    },
+    {
+      img: image3,
+      title: 'Teclado Genius',
+      model: 'KB-116',
+    },
+    {
+       img: image4,
+       title: 'Teclado Genius',
+       model: 'KB-118',
+    },
+    {
+      img: image5,
+      title: 'Parlantes Genius',
+      model: 'SW-21-370',
+    },
+   {
+      img: image6,
+      title: 'Parlantes Genius',
+      model: 'SW-21-375'
+   },
+  ]
+  export default tileData;
+ 
+
