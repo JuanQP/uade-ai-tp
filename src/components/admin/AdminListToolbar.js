e6ca9e11bf2b2b8649d2cfc2d13bc4f607d28162
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-05-06 18:28:01 -0300

    Create AdminListToolbar.js

---
+++ src/components/admin/AdminListToolbar.js
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon
+} from '@material-ui/core';
+import { Search as SearchIcon } from 'react-feather';
+
+const AdminListToolbar = (props) => (
+  <Box {...props}>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end'
+      }}
+    >
+      <Button>
+        Editar
+      </Button>
+      <Button sx={{ mx: 1 }}>
+        Eliminar
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+      >
+        Agregar Producto
+      </Button>
+    </Box>
+    <Box sx={{ mt: 3 }}>
+      <Card>
+        <CardContent>
+          <Box sx={{ maxWidth: 500 }}>
+            <TextField
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SvgIcon
+                      fontSize="small"
+                      color="action"
+                    >
+                      <SearchIcon />
+                    </SvgIcon>
+                  </InputAdornment>
+                )
+              }}
+              placeholder="Buscar Producto"
+              variant="outlined"
+            />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  </Box>
+);
+
+export default AdminListToolbar;
