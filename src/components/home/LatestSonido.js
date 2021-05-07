c1c3ebb2c4ab8504fa63f990581923980f04bb90
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-05-07 15:32:38 -0300

    Create LatestSonido.js

---
+++ src/components/home/LatestSonido.js
@@ -0,0 +1,66 @@
+import {
+    Box,
+    Button,
+    Card,
+    CardHeader,
+    Divider,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText
+  } from '@material-ui/core';
+  import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+  import products from 'src/__mocks__/ABMlist';
+  import { Link as RouterLink } from 'react-router-dom';
+  
+  const LatestSonido = (props) => (
+    <Card {...props}>
+      <CardHeader
+        title="Disfrute lo mejor en audio"
+      />
+      <Divider />
+      <List>
+        {products.slice(4, 8).map((product, i) => (
+          <ListItem
+            divider={i < products.length - 1}
+            key={product.id}
+          >
+            <ListItemAvatar>
+              <img
+                alt={`${product.producto} ${product.marca} ${product.modelo}`}
+                src={product.img}
+                style={{
+                  height: 100,
+                  width: 140
+                }}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={`${product.producto} ${product.marca} ${product.modelo}`}
+            />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          p: 2
+        }}
+      >
+       <Button
+        color="primary"
+        endIcon={<ArrowRightIcon />}
+        size="small"
+        variant="text"
+        component={RouterLink} to={'/app/products'}
+      >
+        Ver Catalogo completo
+      </Button>
+      </Box>
+    </Card>
+  );
+  
+  export default LatestSonido;
+  
