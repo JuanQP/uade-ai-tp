6e78c5cac8aa42e55462026ab7fae411c0f7c786
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-06-29 00:42:30 -0300

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -105,7 +105,7 @@ const ProductDetail = (props) => {
                 }}
               >
                 <Avatar
-                  src={utils.productPath(product.img)}
+                  src={product.img}
                   alt="Product"
                   variant="square"
                   align="center"
