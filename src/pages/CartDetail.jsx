12f322194ba2615981c6bcbc3438bc9ccd2c9895
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-06-30 19:52:08 -0300

    Update CartDetail.jsx

--- src/pages/CartDetail.jsx
+++ src/pages/CartDetail.jsx
@@ -1,128 +1,232 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
-  Alert,
+  Avatar,
   Box,
   Button,
   Card,
-  CardActions,
   CardContent,
-  CardHeader,
+  CircularProgress,
   Container,
-  Hidden,
-  List,
-  Slide,
-  Snackbar,
+  FormControl,
+  FormHelperText,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
   Typography,
 } from '@material-ui/core';
-import CartDetailItem from './CartDetailItem';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import { Link as RouterLink } from 'react-router-dom';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import BrokenImageIcon from '@material-ui/icons/BrokenImage';
+import { useParams } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import ClickSparkle from 'src/components/ClickSparkle';
+import axios from 'axios';
 
-function SlideTransition(props) {
-  return <Slide {...props} direction="up" />;
+const useStyles = makeStyles((theme) => ({
+  magicButton: {
+    height: '100%',
+    '&:hover': {
+      animation: `$myEffect 250ms ${theme.transitions.easing.easeInOut}`,
+      animationFillMode: 'forwards',
+    }
+  },
+  "@keyframes myEffect": {
+    "0%": {
+    },
+    "100%": {
+      background: 'linear-gradient(45deg, #00b09e 30%, #79fa6e 90%)',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+    }
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+function unidadesItems(cantidad) {
+  const numeros = [...Array(Math.min(cantidad, 10)).keys()];
+  return numeros.map((n) => <MenuItem key={`item-${n+1}}`} value={n+1}>{`${n+1} ${(n+1) === 1 ? 'unidad': 'unidades'}`}</MenuItem>);
 }
 
-const CartDetail = (props) => {
-
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
+const ProductDetail = (props) => {
+  const classes = useStyles();
+  const { product_id } = useParams();
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [waitingServer, setWaitingServer] = useState(true);
 
   useEffect(() => {
-    if(!props.user.isGuest) {
-      return () => {};
-    }
-    const timer = setTimeout(() => {
-      setSnackbarOpen(true);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, [props.user.isGuest]);
+    axios.get('http://localhost:4000/products/detail/' + product_id)
+    .then((res) => {
+      setProduct(res.data.data);
+    })
+    .finally(() => {
+      setWaitingServer(false);
+    });
+  }, [product_id]);
 
-  function handleClose() {
-    setSnackbarOpen(false);
+  function handleChange(e) {
+    setQuantity(e.target.value);
   }
 
-  function handleAddUnit(item) {
-    props.onAddProduct(item.product);
+  function handleAgregarClick() {
+    props.onAgregarClick(product, quantity);
   }
 
-  function handleMinusUnit(item) {
-    props.onMinusProduct(item.product);
-  }
-
-  function handleRemoveProduct(item) {
-    props.onRemoveProduct(item.product);
+  if(waitingServer) {
+    return (
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center'}}
+      >
+        <CircularProgress color="inherit" />
+      </Box>
+    );
   }
 
   return (
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        minHeight: '100%',
-        py: 3
-      }}
-    >
-      <Container maxWidth="lg">
-        <Card>
-          <CardHeader
-            title="Resumen del Carrito"
-            subheader="Acá podés ver todos los productos que elegiste 😁"
-          />
-          {props.products.length === 0 ?
-            // Si no hay productos...
-            <CardContent>
-              <Alert severity="info">Todavía no seleccionaste ningún producto. <RouterLink to="/app/products">¿Qué estás esperando? 😁</RouterLink></Alert>
-            </CardContent> :
-            // Si hay productos...
-            <>
-            <CardContent>
-              <Hidden lgUp>
-                <Typography variant="subtitle2">
-                  Para modificar las cantidades, presioná la imagen del producto que quieras editar.
-                </Typography>
-              </Hidden>
-              <List>
-                {props.products.map(p => (
-                  <CartDetailItem
-                    key={p.product.id}
-                    item={p}
-                    onAddUnit={handleAddUnit}
-                    onMinusUnit={handleMinusUnit}
-                    onRemoveProduct={handleRemoveProduct}
-                  />
-                ))}
-              </List>
-              <Typography variant="h3" align="right">
-                Total ${props.products.map(p => p.product.precio * p.quantity).reduce((a,b) => (a+b), 0)}
-              </Typography>
-            </CardContent>
-            <CardActions sx={{justifyContent: 'right'}}>
-              <Button
-                variant="contained"
-                endIcon={<ChevronRightIcon />}
-                component={RouterLink}
-                to='/app/checkout'
-              >
-                Ir al pago
-              </Button>
-            </CardActions>
-            </>
-          }
-        </Card>
-      </Container>
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
+    <Container maxWidth="lg">
+      <Card
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
         }}
-        open={snackbarOpen}
-        onClose={handleClose}
-        TransitionComponent={SlideTransition}
       >
-        <Alert onClose={handleClose} severity="info">
-          No estás logeado. <RouterLink to="/login">¿Tenés una cuenta? Ingresá</RouterLink>. Hacer las compras es más fácil si estás registrado 👌.
-        </Alert>
-      </Snackbar>
-    </Box>
+        <CardContent>
+          <Grid sx={{marginTop: 3}} container spacing={3}>
+            <Grid sx={{width: '100%'}} item md={6} sm={12}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  pb: 3
+                }}
+              >
+                <Avatar
+                  src={product.img}
+                  alt="Product"
+                  variant="square"
+                  align="center"
+                  sx={{
+                    height: 170,
+                    width: 230
+                  }}
+                >
+                  <BrokenImageIcon />
+                </Avatar>
+              </Box>
+            </Grid>
+            <Grid align="center" item md={6} sm={12}>
+              <Typography
+                color="textPrimary"
+                gutterBottom
+                variant="h1"
+              >
+                {product.nombre}
+              </Typography>
+              <Typography
+                color="#009688"
+                variant="subtitle1"
+                sx={{fontSize: 30}}
+                mb={2}
+              >
+                $ {product.precio}
+              </Typography>
+              <Grid container sx={{justifyContent: 'center'}}>
+                <Grid item md={6} sm={12}>
+                  <FormControl variant="outlined" className={classes.formControl}>
+                    <InputLabel id="demo-simple-select-outlined-label">Cantidad</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-outlined-label"
+                      id="demo-simple-select-outlined"
+                      value={quantity}
+                      onChange={handleChange}
+                      label="Cantidad"
+                    >
+                      {unidadesItems(product.stock)}
+                    </Select>
+                    <FormHelperText>Unidades disponibles: {product.stock}</FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid item md={6} sm={12}>
+                  <ClickSparkle style={{height: '100%'}} text={`+${quantity}`}>
+                    <Button
+                      className={classes.magicButton}
+                      variant="contained"
+                      color="primary"
+                      startIcon={<AddShoppingCartIcon />}
+                      onClick={handleAgregarClick}
+                    >
+                      Agregar al carrito
+                    </Button>
+                  </ClickSparkle>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid sx={{marginTop: 3}} container spacing={3}>
+            <Grid item xs={6} md={3}>
+              <Typography color="textPrimary" variant="h4">Marca</Typography>
+              <Typography
+                color="textPrimary"
+                variant="body1"
+              >
+                {product.marca}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <Typography color="textPrimary" variant="h4">Modelo</Typography>
+              <Typography
+                color="textPrimary"
+                variant="body1"
+              >
+                {product.modelo}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <Typography color="textPrimary" variant="h4">Interfaz</Typography>
+              <Typography
+                color="textPrimary"
+                variant="body1"
+              >
+                {product.interfaz}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <Typography color="textPrimary" variant="h4">Peso</Typography>
+              <Typography
+                color="textPrimary"
+                variant="body1"
+              >
+                {product.peso}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid sx={{marginTop: 3}} container spacing={3}>
+            <Grid item xs={12}>
+            <Typography color="textPrimary" variant="h4">Descripción</Typography>
+            {product.descripcion === '' ?
+              <Typography
+                color="textSecondary"
+                variant="body1"
+              >
+                <em>No se proporcionó una descripción</em>
+              </Typography>
+              :
+              <Typography
+                color="textPrimary"
+                variant="body1"
+              >
+                {product.descripcion}
+              </Typography>
+            }
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Container>
   )
 }
 
-export default CartDetail;
+export default ProductDetail;
