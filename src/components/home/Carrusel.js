884de14d40394ac76d1753fba4296a5bbc357d57
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-06-30 18:53:50 -0300

    Update Carrusel.js

--- src/components/home/Carrusel.js
+++ src/components/home/Carrusel.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 import {
   Container,
   Box,
 } from '@material-ui/core';
 
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
 const tutorialSteps = [
   {
     imgPath:
-      '/static/images/noesperes.jpg',
+    '/static/images/noesperes.jpg',
   },
   {
     imgPath:
-      '/static/images/hotsale.jpg',
-  },
-  {
-    imgPath:
-    '/static/images/descuento.jpg',
+    '/static/images/hotsale.jpg',
   },
 ];
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      maxWidth: '100%',
-      flexGrow: 1,
-    },
+  root: {
+    maxWidth: '80%',
+    flexGrow: 1,
+  },
   header: {
     display: 'flex',
     alignItems: 'center',
@@ -39,66 +39,80 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.default,
   },
   img: {
-    height: '100%',
-    Width: '100%',
+    height: '90%',
     overflow: 'hidden',
     display: 'block',
     width: '100%',
   },
 }));
 
-export default function Carrusel() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const maxSteps = tutorialSteps.length;
-  
-    const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-  
-    const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
+function Carrusel() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = tutorialSteps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
   return (
-    <Box
-      sx={{
+	<Box
+     		 sx={{
       
-        minHeight: '100%',
-        py: 3
-      }}
-    >
-    <Container maxWidth="xl">
+        	minHeight: '100',
+        	py: 3
+      	}}
+      >
+     <Container maxWidth="xl" align ="center">
     <div className={classes.root}>
-    <Paper square elevation={0} className={classes.header}>
-      <Typography>{tutorialSteps[activeStep].label}</Typography>
-    </Paper>
-    <img
-      className={classes.img}
-      src={tutorialSteps[activeStep].imgPath}
-      alt="imag"
-    />
-    <MobileStepper
-      variant="dots"
-      steps={maxSteps}
-      position="static"
-      activeStep={activeStep}
-      className={classes.root}
-      nextButton={
-        <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-          Próximo
-          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-        </Button>
-      }
-      backButton={
-        <Button size="small" onClick={handleBack} disabled={activeStep === 0 }>
-          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-          Anterior
-        </Button>
+      <Paper square elevation={0} className={classes.header}>
+        <Typography>{tutorialSteps[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {tutorialSteps.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <img className={classes.img} src={step.imgPath} alt={step.label} />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        variant="dots"
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            Próximo
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Anterior
+          </Button>
         }
       />
-      </div>
-      </Container>
-      </Box>
+    </div>
+   </Container>
+   </Box>
   );
 }
+
+export default Carrusel;
