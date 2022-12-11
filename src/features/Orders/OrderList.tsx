b4c11af5c56fb2b49f50687d8b5a814be354ee06
JuanQP <quinterosp.juan@gmail.com>
2022-12-11 15:36:54 -0300

    Make order table scrollable
    It's better for mobile

--- src/features/Orders/OrderList.tsx
+++ src/features/Orders/OrderList.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Table } from "@mantine/core";
+import { ActionIcon, ScrollArea, Table } from "@mantine/core";
 import { IconEye } from "@tabler/icons";
 import { Link } from "react-router-dom";
 
@@ -8,31 +8,33 @@ interface Props {
 
 export function OrderList({ orders }: Props) {
   return (
-    <Table striped highlightOnHover>
-      <thead>
-        <tr>
-          <th>Fecha de Compra</th>
-          <th>Fecha de Entrega</th>
-          <th>Monto</th>
-          <th>Estado</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map(order => (
-          <tr key={order._id}>
-            <td>{order.orderDate}</td>
-            <td>{order.deliveryDate}</td>
-            <td>{order.total}</td>
-            <td>{order.status}</td>
-            <td>
-              <ActionIcon color="blue" component={Link} to={`/order/${order._id}`}>
-                <IconEye />
-              </ActionIcon>
-            </td>
+    <ScrollArea style={{ width: '100%', height: '100%' }}>
+      <Table striped highlightOnHover>
+        <thead>
+          <tr>
+            <th>Fecha de Compra</th>
+            <th>Fecha de Entrega</th>
+            <th>Monto</th>
+            <th>Estado</th>
+            <th>Acciones</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {orders.map(order => (
+            <tr key={order._id}>
+              <td>{order.orderDate}</td>
+              <td>{order.deliveryDate}</td>
+              <td>{order.total}</td>
+              <td>{order.status}</td>
+              <td>
+                <ActionIcon color="blue" component={Link} to={`/order/${order._id}`}>
+                  <IconEye />
+                </ActionIcon>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </ScrollArea>
   )
 }
